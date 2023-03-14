8684b304d11c163fb6304ec46742c91f8ab18a9f
Zahir Mahmud <mihonmahmud@gmail.com>
2023-03-14 21:44:39 +0600

    Minor UI improvements

--- src/components/AggregationPipelineEditor/AggregationPipelineEditor.vue
+++ src/components/AggregationPipelineEditor/AggregationPipelineEditor.vue
@@ -15,12 +15,20 @@
 
             <div v-if="stagesQuery.length > 0" class="w-full flex flex-col gap-3 h-60 overflow-y-auto">
                 <div v-for="(stage, index) in stagesQuery" :key="index" class="flex gap-2">
-                    <n-checkbox
-                        size="large"
-                        v-model:checked="stage.shouldApply"
-                        @update:checked="parseAndTriggerAggregation"
-                    >
-                    </n-checkbox>
+                    <div class="flex flex-col items-center">
+                        <n-checkbox
+                            size="large"
+                            v-model:checked="stage.shouldApply"
+                            @update:checked="parseAndTriggerAggregation"
+                        >
+                        </n-checkbox>
+
+                        <svg @click="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 mt-2 text-red-500 hover:cursor-pointer">
+                            <path fill-rule="evenodd"
+                                d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z"
+                                clip-rule="evenodd" />
+                        </svg>
+                    </div>
 
                     <div class="w-full">
                         <JSONView :editor-value="JSON.parse(stage.query)" />
@@ -69,41 +77,49 @@
 
             <div v-if="showLookupSection" class="mt-3">
                 <div class="flex gap-2">
-                    <n-select
-                        size="small"
-                        v-model:value="lookUpModel.from"
-                        filterable
-                        :options="AggregationBuilderService.getOtherCollectionNames(props.dbName, props.collectionName)"
-                        :render-option="NaiveUiService.renderOption"
-                        :placeholder="'From'"
-                        @update:value="handleForeignCollectionSelect"
-                    />
+                    <div class="grow flex-col">
+                        <p class="text-xs mb-1">From</p>
+                        <n-select
+                            size="small"
+                            v-model:value="lookUpModel.from"
+                            filterable
+                            :options="AggregationBuilderService.getOtherCollectionNames(props.dbName, props.collectionName)"
+                            :render-option="NaiveUiService.renderOption"
+                            @update:value="handleForeignCollectionSelect"
+                        />
+                    </div>
 
-                    <n-select
-                        size="small"
-                        v-model:value="lookUpModel.foreignField"
-                        filterable
-                        :options="foreignFields"
-                        :render-option="NaiveUiService.renderOption"
-                        :placeholder="'Foreign Field'"
-                    />
+                    <div class="grow flex-col">
+                        <p class="text-xs mb-1">Foreign Field</p>
+                        <n-select
+                            size="small"
+                            v-model:value="lookUpModel.foreignField"
+                            filterable
+                            :options="foreignFields"
+                            :render-option="NaiveUiService.renderOption"
+                        />
+                    </div>                   
                 </div>
 
-                <div class="flex gap-2 mt-2">
-                    <n-select
-                        size="small"
-                        v-model:value="lookUpModel.localField"
-                        filterable
-                        :options="localFields"
-                        :render-option="NaiveUiService.renderOption"
-                        :placeholder="'From'"
-                    />
+                <div class="flex basis-0 gap-2 mt-2">
+                    <div class="grow flex-col">
+                        <p class="text-xs mb-1">Local Field</p>
+                        <n-select
+                            size="small"
+                            v-model:value="lookUpModel.localField"
+                            filterable
+                            :options="localFields"
+                            :render-option="NaiveUiService.renderOption"
+                        />
+                    </div>
 
-                    <n-input
-                        size="small"
-                        v-model:value="lookUpModel.as"
-                        :placeholder="'As'"
-                    />
+                    <div class="grow flex-col">
+                        <p class="text-xs mb-1">As</p>
+                        <n-input
+                            size="small"
+                            v-model:value="lookUpModel.as"
+                        />
+                    </div>                   
                 </div>
 
                 <div class="flex justify-end mt-2 gap-2">
@@ -114,7 +130,8 @@
             </div>
 
             <div v-if="showProjectSection" class="mt-3">
-                <div class="flex">
+                <div class="flex flex-col">
+                    <p class="text-xs mb-1">Fields</p>
                     <n-select
                         size="small"
                         v-model:value="projectedFields"
@@ -122,8 +139,7 @@
                         multiple
                         :options="localFields"
                         :render-option="NaiveUiService.renderOption"
-                        :placeholder="'Select Fields'"
-                    />
+                    />                
                 </div>
 
                 <div class="flex justify-end mt-2 gap-2">
@@ -134,7 +150,8 @@
             </div>
 
             <div v-if="showUnsetSection" class="mt-3">
-                <div class="flex">
+                <div class="flex flex-col">
+                    <p class="text-xs mb-1">Fields</p>
                     <n-select
                         size="small"
                         v-model:value="unsetFields"
@@ -142,7 +159,6 @@
                         multiple
                         :options="localFields"
                         :render-option="NaiveUiService.renderOption"
-                        :placeholder="'Select Fields'"
                     />
                 </div>
 
@@ -155,6 +171,7 @@
 
             <div v-if="showUnwindSection" class="mt-3">
                 <div class="flex flex-col gap-2">
+                    <p class="text-xs mb-1">Field</p>
                     <n-select
                         size="small"
                         v-model:value="unwindModel.field"
