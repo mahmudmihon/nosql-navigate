63e7d7015cfc3ae7b834a05b2fb71ba983439c1f
Zahir Mahmud <mihonmahmud@gmail.com>
2023-04-09 22:23:44 +0600

    In app db update

--- src-tauri/src/services/sql_lite_service.rs
+++ src-tauri/src/services/sql_lite_service.rs
@@ -1,5 +1,6 @@
 use std::path::PathBuf;
 use rusqlite::{Connection, Result};
+use uuid::Uuid;
 
 use crate::models::dtos::ImportExportSummary;
 
@@ -13,9 +14,9 @@ fn get_db_path() -> PathBuf {
 }
 
 pub fn create_initial_tables() -> Result<()> {
-    let dbPath = get_db_path();
+    let db_path = get_db_path();
 
-    let conn = Connection::open(dbPath)?;
+    let conn = Connection::open(db_path)?;
 
     conn.execute(
         "CREATE TABLE IF NOT EXISTS saved_connections
@@ -33,6 +34,7 @@ pub fn create_initial_tables() -> Result<()> {
             id TEXT NOT NULL,
             db_name TEXT NOT NULL,
             collection_name TEXT NOT NULL,
+            path TEXT NOT NULL,
             operation_type TEXT NOT NULL,
             operation_status TEXT NOT NULL,
             documents_count INTEGER NOT NULL,
@@ -44,27 +46,55 @@ pub fn create_initial_tables() -> Result<()> {
     return Ok(());
 }
 
-pub fn insert_import_export_summary(summary: &ImportExportSummary) -> Result<()> {
-    let dbPath = get_db_path();
+pub fn save_db_connection(connection_name: &str, connection_url: &str) -> Result<()> {
+    let db_path = get_db_path();
 
-    let conn = Connection::open(dbPath)?;
+    let conn = Connection::open(db_path)?;
+
+    let id = Uuid::new_v4().to_string();
 
     conn.execute(
-        "INSERT INTO import_export_summary (id, db_name, collection_name, operation_type, operation_status, documents_count, created_on) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7)",
-        (&summary.id, &summary.db_name, &summary.collection_name, &summary.operation_type, &summary.operation_status, &summary.documents_count, &summary.created_on)
+        "INSERT INTO saved_connections (id, connection_name, connection_url) VALUES (?1, ?2, ?3)",
+        (id, connection_name, connection_url)
+    )?;
+
+    return Ok(());
+}
+
+pub fn delete_db_connection(id: &str) -> Result<()> {
+    let db_path = get_db_path();
+
+    let conn = Connection::open(db_path)?;
+
+    conn.execute(
+        "DELETE FROM saved_connections WHERE id=?1", 
+        [id]
+    )?;
+
+    return Ok(());
+}
+
+pub fn insert_import_export_summary(summary: &ImportExportSummary) -> Result<()> {
+    let db_path = get_db_path();
+
+    let conn = Connection::open(db_path)?;
+
+    conn.execute(
+        "INSERT INTO import_export_summary (id, db_name, collection_name, path, operation_type, operation_status, documents_count, created_on) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8)",
+        (summary.id, summary.db_name, summary.collection_name, summary.path, summary.operation_type, summary.operation_status, summary.documents_count, summary.created_on)
     )?;
 
     return Ok(());
 }
 
 pub fn update_import_export_summary(summary: &ImportExportSummary) -> Result<()> {
-    let dbPath = get_db_path();
+    let db_path = get_db_path();
 
-    let conn = Connection::open(dbPath)?;
+    let conn = Connection::open(db_path)?;
 
     conn.execute(
         "UPDATE import_export_summary SET operation_status=?1, documents_count=?2 WHERE id=?3",
-        (&summary.operation_status, &summary.documents_count, &summary.id)
+        (summary.operation_status, summary.documents_count, summary.id)
     )?;
 
     return Ok(());
